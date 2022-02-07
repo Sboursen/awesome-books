c5323b276e526c962b010998727a7361cf519c6d
amira <amiraabouhadid@gmail.com>
2022-02-07 23:26:11 +0200

    Remove linter errors

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
-
-let container = document.getElementById('container');
-
-books.map((book, index) => {
-  displayBook(book, index);
-});
-
-function displayBook(book, index) {
-  let bookContainer = document.createElement('div');
-  bookContainer.id = index;
-
-  let text = document.createElement('p');
-  text.innerHTML =
-    'Title: ' +
-    book.title +
-    '<br>' +
-    'Author: ' +
-    book.author +
-    '<br>';
-  bookContainer.append(text);
-
-  let removeButton = document.createElement('button');
-  removeButton.innerHTML = 'Remove';
-  removeButton.onclick = (e) => {
-    removeBook(book, index);
-  };
-  bookContainer.append(removeButton);
-  let breakline = document.createElement('hr');
-  bookContainer.append(breakline);
-  container.append(bookContainer);
-}
+const container = document.getElementById('container');
 
 function removeBook(book, index) {
-  let bookContainer = document.getElementById(index);
-  books = books.filter((el, i) => el !== book);
+  const bookContainer = document.getElementById(index);
+  books = books.filter((el) => el !== book);
   localStorage.setItem('books', JSON.stringify(books));
   container.removeChild(bookContainer);
 }
 
-// add book
+function displayBook(book, index) {
+  const bookContainer = document.createElement('div');
+  bookContainer.id = index;
+
+  const text = document.createElement('p');
+  text.innerHTML = `Title: ${book.title}<br> Author: ${book.author}<br>`;
+  bookContainer.append(text);
+
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = 'Remove';
+  removeButton.onclick = () => {
+    removeBook(book, index);
+  };
+  bookContainer.append(removeButton);
+  const breakline = document.createElement('hr');
+  bookContainer.append(breakline);
+  container.append(bookContainer);
+}
+books.map((book, index) => {
+  displayBook(book, index); return '';
+});
+
 const titleInput = document.querySelector('#title');
 const authorInput = document.querySelector('#author');
 const addButton = document.querySelector('.add-book');
@@ -52,11 +43,9 @@ function addBook() {
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
 
-
-  document.querySelector('#author').value = "";
-  document.querySelector('#title').value = "";
+  document.querySelector('#author').value = '';
+  document.querySelector('#title').value = '';
   displayBook(book, books.length - 1);
-
 }
 
 addButton.addEventListener('click', addBook);
