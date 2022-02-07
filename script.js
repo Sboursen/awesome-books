a526e452acede60f58d2e9c855aa12e028005b3e
sboursen <dev.sboursen@gmail.com>
2022-02-07 22:01:26 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addButton = document.querySelector('.add-book');
 
-
-let container = document.getElementById('container');
-
-books.map((book, index) => {
-  displayBook(book, index);
-});
-
-function displayBook(book, index) {
-  let bookContainer = document.createElement('div');
-  bookContainer.id = index;
-
-  let text = document.createElement('p');
-  text.innerHTML =
-    'Title: ' +
-    book.title +
-    '<br>' +
-    'Author: ' +
-    book.author +
-    '<br>';
-  bookContainer.append(text);
-
-  let removeButton = document.createElement('button');
-  removeButton.innerHTML = 'Remove';
-  removeButton.onclick = (e) => {
-    removeBook(book, index);
-  };
-  bookContainer.append(removeButton);
-  let breakline = document.createElement('hr');
-  bookContainer.append(breakline);
-  container.append(bookContainer);
-}
+const container = document.getElementById('container');
 
 function removeBook(book, index) {
-  let bookContainer = document.getElementById(index);
-  books = books.filter((el, i) => el !== book);
+  const bookContainer = document.getElementById(index);
+  books = books.filter((el) => el !== book);
   localStorage.setItem('books', JSON.stringify(books));
   container.removeChild(bookContainer);
 }
 
-// add book
-const titleInput = document.querySelector('#title');
-const authorInput = document.querySelector('#author');
-const addButton = document.querySelector('.add-book');
+function displayBook(book, index) {
+  const bookContainer = document.createElement('div');
+  bookContainer.id = index;
+
+  const text = document.createElement('p');
+  text.innerHTML = `Title: ${book.title}<br>`
+    + `Author: ${book.author}<br>`;
+  bookContainer.append(text);
+
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = 'Remove';
+  removeButton.onclick = () => {
+    removeBook(book, index);
+  };
+  bookContainer.append(removeButton);
+  const breakline = document.createElement('hr');
+  bookContainer.append(breakline);
+  container.append(bookContainer);
+}
+
+books.forEach((book, index) => {
+  displayBook(book, index);
+});
 
 function addBook() {
   const book = {
@@ -52,11 +44,9 @@ function addBook() {
   books.push(book);
   localStorage.setItem('books', JSON.stringify(books));
 
-
-  document.querySelector('#author').value = "";
-  document.querySelector('#title').value = "";
+  document.querySelector('#author').value = '';
+  document.querySelector('#title').value = '';
   displayBook(book, books.length - 1);
-
 }
 
 addButton.addEventListener('click', addBook);
