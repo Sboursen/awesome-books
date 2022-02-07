4a0a7ea974d3ff3383e85b9a1bb4eec983a45f1d
amira <amiraabouhadid@gmail.com>
2022-02-07 16:08:52 +0200

    Add displayBook and removeBook functions

--- script.js
+++ script.js
@@ -5,6 +5,44 @@ let books = [{
   title: "jacobs",
   author: "marks"
 }, {
-  title:"ajkasn",
+  title: "ajkasn",
   author: "hehbj"
 }];
+
+
+let container = document.getElementById('container')
+
+books.map((book, index) => {
+  displayBook(book, index)
+})
+
+function displayBook(book, index){
+  let bookContainer = document.createElement('div');
+  bookContainer.id = index;
+
+  let text = document.createElement('p');
+  text.innerHTML = "Title: " + book.title + "<br>" + "Author: " + book.author + "<br>";
+  bookContainer.append(text);
+
+  let removeButton = document.createElement('button');
+  removeButton.innerHTML = "Remove";
+  removeButton.onclick = (e) => {
+    removeBook(book, index)
+  };
+  bookContainer.append(removeButton);
+  let breakline = document.createElement('hr');
+  bookContainer.append(breakline);
+  container.append(bookContainer);
+}
+
+function removeBook(book, index){
+  let bookContainer = document.getElementById(index);
+  books.splice(index, index+1);
+  container.removeChild(bookContainer);
+
+}
+
+function addBook() {
+
+
+}
