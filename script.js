326446c4fadb44e4429ff46c5d38f45409c21b2d
sboursen <dev.sboursen@gmail.com>
2022-02-07 15:32:54 +0100

    Implement Add book functionality

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-let books = [{
-  title: 'mockingbird',
-  author: 'steve'
-}, {
-  title: "jacobs",
-  author: "marks"
-}, {
-  title: "ajkasn",
-  author: "hehbj"
-}];
+let books = [
+  {
+    title: 'mockingbird',
+    author: 'steve',
+  },
+  {
+    title: 'jacobs',
+    author: 'marks',
+  },
+  {
+    title: 'ajkasn',
+    author: 'hehbj',
+  },
+];
 
-
-let container = document.getElementById('container')
+let container = document.getElementById('container');
 
 books.map((book, index) => {
-  displayBook(book, index)
-})
+  displayBook(book, index);
+});
 
-function displayBook(book, index){
+function displayBook(book, index) {
   let bookContainer = document.createElement('div');
   bookContainer.id = index;
 
   let text = document.createElement('p');
-  text.innerHTML = "Title: " + book.title + "<br>" + "Author: " + book.author + "<br>";
+  text.innerHTML =
+    'Title: ' +
+    book.title +
+    '<br>' +
+    'Author: ' +
+    book.author +
+    '<br>';
   bookContainer.append(text);
 
   let removeButton = document.createElement('button');
-  removeButton.innerHTML = "Remove";
+  removeButton.innerHTML = 'Remove';
   removeButton.onclick = (e) => {
-    removeBook(book, index)
+    removeBook(book, index);
   };
   bookContainer.append(removeButton);
   let breakline = document.createElement('hr');
@@ -35,14 +44,23 @@ function displayBook(book, index){
   container.append(bookContainer);
 }
 
-function removeBook(book, index){
+function removeBook(book, index) {
   let bookContainer = document.getElementById(index);
-  books.splice(index, index+1);
+  books.splice(index, index + 1);
   container.removeChild(bookContainer);
-
 }
 
+// add book
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const addButton = document.querySelector('.add-book');
 function addBook() {
-
-
+  const book = {
+    title: titleInput.value,
+    author: authorInput.value,
+  };
+  books.push(book);
+  displayBook(book, books.length - 1);
 }
+
+addButton.addEventListener('click', addBook);
