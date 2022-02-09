6adfd7c1a075c6807e04497e6abf7e9624b8c5c2
sboursen <dev.sboursen@gmail.com>
2022-02-09 13:37:06 +0100

    Add the mobile menu with responsive functionalities

--- script.js
+++ script.js
@@ -45,11 +45,13 @@ class UserInterface {
     text.innerHTML = `"${book.title}" by ${book.author}`;
 
     bookContainer.append(text);
-    const removeButtonContainer = document.createElement('td');
+    const removeButtonContainer =
+      document.createElement('td');
 
     const removeButton = document.createElement('button');
     removeButton.classList.add('remove-button');
-    removeButton.innerHTML = "<i class='fas fa-trash-alt'></i> Remove";
+    removeButton.innerHTML =
+      "<i class='fas fa-trash-alt'></i> Remove";
 
     removeButton.onclick = () => {
       UserInterface.removeBook(book, index);
@@ -78,3 +80,66 @@ UserInterface.addButton.addEventListener(
   'click',
   UserInterface.addBook,
 );
+
+// ||| Hide and show mobile menu
+
+const MEDIA_BREAKPOINT = 768;
+const mobileMenuButton = document.querySelector(
+  'button.mobile-menu-button',
+);
+const mobileMenuList = document.querySelectorAll(
+  '.mobile-menu .page-navigation > li',
+);
+const mobileMenu = document.querySelector(
+  'div.mobile-menu',
+);
+
+const cancelMobileMenu = document.querySelector(
+  '.mobile-menu .cancel',
+);
+
+function showMobileMenu(e) {
+  if (
+    e.currentTarget.classList.contains('mobile-menu-button')
+  ) {
+    mobileMenu.style.display = 'flex';
+    mobileMenu.style['z-index'] = 2;
+    document.body.style.overflowY = 'hidden';
+  }
+}
+
+function hideMobileMenu(e) {
+  if (
+    e.currentTarget.classList.contains('cancel') ||
+    e.currentTarget.parentNode.classList.contains(
+      'mobile-list',
+    )
+  ) {
+    mobileMenu.style.display = 'none';
+    mobileMenu.style['z-index'] = -2;
+    document.body.style.overflowY = 'scroll';
+  }
+}
+
+function hideMobileMenuOnEvent(e) {
+  if (mobileMenu.style.display !== 'none') {
+    if (e.type === 'resize') {
+      if (window.innerWidth > MEDIA_BREAKPOINT) {
+        mobileMenu.style.display = 'none';
+        mobileMenu.style['z-index'] = -2;
+        document.body.style.overflowY = 'scroll';
+      }
+    } else {
+      mobileMenu.style.display = 'none';
+      mobileMenu.style['z-index'] = -2;
+      document.body.style.overflowY = 'scroll';
+    }
+  }
+}
+
+mobileMenuButton.addEventListener('click', showMobileMenu);
+cancelMobileMenu.addEventListener('click', hideMobileMenu);
+mobileMenuList.forEach((node) =>
+  node.addEventListener('click', hideMobileMenu),
+);
+window.addEventListener('resize', hideMobileMenuOnEvent);
